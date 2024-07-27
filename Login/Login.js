35fe5714fd3dca7a8169c72d770ba668c7f0fa99
Nguyen <nguyenmandat000@gmail.com>
2024-07-28 03:05:14 +0700

    add remember and forgot at login add function generate password for register

--- Login/Login.js
+++ Login/Login.js
@@ -1,8 +1,9 @@
+//  Switch login and register interface
 const signUp = document.querySelector("#signUp");
 const signIn = document.querySelector("#signIn");
-const passwordIcon = document.querySelectorAll('.password__icon')
-const authPassword = document.querySelectorAll('.auth__password')
-const toggleBtn = document.querySelector("#themeToggleButton")
+const passwordIcon = document.querySelectorAll('.password__icon');
+const authPassword = document.querySelectorAll('.auth__password');
+// const toggleBtn = document.querySelector("#themeToggleButton")
 
 // when click sign up button
 signUp.addEventListener('click', () => {
@@ -33,3 +34,50 @@ for (var i = 0; i < passwordIcon.length; ++i) {
         }
     });
 }
+
+/*--------------------------Generate Password for Register----------------------*/
+
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeSymbols = true;
+const includeLNumber = true;
+const generateIcon = document.querySelector(".generate_icon");
+
+function generateRandomPassword(length, lowerCase, upperCase, symbol, number) {
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()+><.?/";
+
+    let availableChars ="";
+    let password ="";
+
+    availableChars += lowerCase ? lowerCaseChars : "";
+    availableChars += upperCase ? upperCaseChars : "";
+    availableChars += number ? numberChars : "";
+    availableChars += symbol ? symbolChars : "";
+
+    if(length <= 0){
+        return (`Password should be more than 1 charater!`);
+    }
+    if(availableChars.length === 0){
+        window.alert("Please set True at least 1 condition!");
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * availableChars.length);
+        password += availableChars[randomIndex];
+    }
+    return password;
+}
+
+function suggesPassword(){
+    const password = generateRandomPassword(passwordLength, includeLowerCase, includeUpperCase, includeSymbols, includeLNumber);
+    const autoPassword = document.querySelector("#res_password");
+    autoPassword.value = password;
+}
+
+generateIcon.addEventListener("click", event => {
+    suggesPassword();
+})
