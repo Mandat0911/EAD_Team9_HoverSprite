a0ab648c73c2a1deee0d40aee09002598b9699ce
Your Name <you@example.com>
2024-09-09 23:41:08 +0700

    #21 -@Service

--- hoversprite/src/main/java/com/example/hoversprite/user/UserService.java
+++ hoversprite/src/main/java/com/example/hoversprite/user/UserService.java
@@ -2,7 +2,9 @@ package com.example.hoversprite.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
